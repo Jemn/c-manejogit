134dd06f1f59888264960df7f17110b04efee1b7
Jemn <juliomateo@outlook.com>
2024-04-04 12:57:53 -0500

    hemos creado la clase Usuario y generado nuestro commit para enviarlo al control de versiones

---
+++ clases-manejogit/src/main/java/com/clases/clasesmanejogit/models/Usuario.java
@@ -0,0 +1,12 @@
+package com.clases.clasesmanejogit.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Usuario {
+
+    private String nombre;
+    private String apellido;
+}
