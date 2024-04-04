bc1d3ff1dece6ec0c6bfd74937bfc4be6301b84d
Jemn <juliomateo@outlook.com>
2024-04-04 12:17:15 -0500

    version inicial

---
+++ clases-manejogit/src/main/java/com/clases/clasesmanejogit/ClasesManejogitApplication.java
@@ -0,0 +1,13 @@
+package com.clases.clasesmanejogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasesManejogitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasesManejogitApplication.class, args);
+	}
+
+}
---
+++ clases-manejogit/src/test/java/com/clases/clasesmanejogit/ClasesManejogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clases.clasesmanejogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasesManejogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
